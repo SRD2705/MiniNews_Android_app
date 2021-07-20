863bdbb1e782688823fb4c6c7b5f483e0c9356de
srd2705 <souravdasrishi@gmail.com>
2021-07-20 17:21:26 +0530

    version 1.2.0 Build Splash screen Build SignIn/SignUp feature Build Logout feature Add loading animation Google login feature available Facebook and Phone login feature not available Firebase account: contactus@mininews@gmail.com

--- app/src/main/java/com/example/newsapp/MainActivity.java
+++ app/src/main/java/com/example/newsapp/MainActivity.java
@@ -1,15 +1,26 @@
 package com.example.newsapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.ProgressBar;
 import android.widget.Toast;
+import android.widget.Toolbar;
+
+import com.airbnb.lottie.LottieAnimationView;
+import com.example.newsapp.databinding.ActivityMainBinding;
+import com.google.firebase.auth.FirebaseAuth;
 
 import java.util.ArrayList;
 
@@ -19,27 +30,45 @@ import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
-public class MainActivity extends AppCompatActivity implements CategoryRVAdapter.CategoryClickInterface{
+public class MainActivity extends AppCompatActivity implements CategoryRVAdapter.CategoryClickInterface {
 
 
-    private RecyclerView newsRV,categoryRV;
+    private RecyclerView newsRV, categoryRV;
     private ProgressBar loadingPB;
     private ArrayList<Articles> articlesArrayList;
     private ArrayList<CategoryRVModal> categoryRVModalArrayList;
     private CategoryRVAdapter categoryRVAdapter;
     private NewsRVAdapter newsRVAdapter;
 
+    LottieAnimationView animationView;
+
+
+    ActivityMainBinding binding;
+    FirebaseAuth auth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+//        setContentView(R.layout.activity_main);
+        setContentView(binding.getRoot());
+
+        setSupportActionBar(binding.idToolbar);
+
+
+        auth = FirebaseAuth.getInstance();
+
+        animationView = findViewById(R.id.mainanimation);
+
+
+
         newsRV = findViewById(R.id.idRVNews);
         categoryRV = findViewById(R.id.idRVCategories);
         loadingPB = findViewById(R.id.idPBLoading);
         articlesArrayList = new ArrayList<>();
         categoryRVModalArrayList = new ArrayList<>();
-        newsRVAdapter = new NewsRVAdapter(articlesArrayList,this);
-        categoryRVAdapter = new CategoryRVAdapter(categoryRVModalArrayList,this,this::onCategoryClick);
+        newsRVAdapter = new NewsRVAdapter(articlesArrayList, this);
+        categoryRVAdapter = new CategoryRVAdapter(categoryRVModalArrayList, this, this::onCategoryClick);
         newsRV.setLayoutManager(new LinearLayoutManager(this));
         newsRV.setAdapter(newsRVAdapter);
         categoryRV.setAdapter(categoryRVAdapter);
@@ -48,28 +77,55 @@ public class MainActivity extends AppCompatActivity implements CategoryRVAdapter
         newsRVAdapter.notifyDataSetChanged();
     }
 
-    private void getCategories(){
-        categoryRVModalArrayList.add(new CategoryRVModal("all_news","https://images.unsplash.com/photo-1586339949916-3e9457bef6d3?ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8bmV3c3xlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"));
-        categoryRVModalArrayList.add(new CategoryRVModal("tech","https://images.unsplash.com/photo-1519389950473-47ba0277781c?ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8dGVjaG5vbG9neXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"));
-        categoryRVModalArrayList.add(new CategoryRVModal("india","https://images.unsplash.com/photo-1532375810709-75b1da00537c?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aW5kaWF8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"));
-        categoryRVModalArrayList.add(new CategoryRVModal("world","https://images.unsplash.com/photo-1531266752426-aad472b7bbf4?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8d29ybGR8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"));
-        categoryRVModalArrayList.add(new CategoryRVModal("sports","https://images.unsplash.com/photo-1587280501635-68a0e82cd5ff?ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8c3BvcnRzfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"));
-        categoryRVModalArrayList.add(new CategoryRVModal("entertainment","https://images.unsplash.com/photo-1470229722913-7c0e2dbbafd3?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8ZW50ZXJ0YWlubWVudHxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"));
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.settings:
+                Toast.makeText(this, "Settings Clicked", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.logout:
+                auth.signOut();
+                Intent intent = new Intent(MainActivity.this,SignInActivity.class);
+                startActivity(intent);
+                finish();
+                break;
+
+        }
+        return true;
+    }
+
+
+    private void getCategories() {
+        categoryRVModalArrayList.add(new CategoryRVModal("all_news", "https://images.unsplash.com/photo-1586339949916-3e9457bef6d3?ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8bmV3c3xlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"));
+        categoryRVModalArrayList.add(new CategoryRVModal("tech", "https://images.unsplash.com/photo-1519389950473-47ba0277781c?ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8dGVjaG5vbG9neXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"));
+        categoryRVModalArrayList.add(new CategoryRVModal("india", "https://images.unsplash.com/photo-1532375810709-75b1da00537c?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aW5kaWF8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"));
+        categoryRVModalArrayList.add(new CategoryRVModal("world", "https://images.unsplash.com/photo-1531266752426-aad472b7bbf4?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8d29ybGR8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"));
+        categoryRVModalArrayList.add(new CategoryRVModal("sports", "https://images.unsplash.com/photo-1587280501635-68a0e82cd5ff?ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8c3BvcnRzfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"));
+        categoryRVModalArrayList.add(new CategoryRVModal("entertainment", "https://images.unsplash.com/photo-1470229722913-7c0e2dbbafd3?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8ZW50ZXJ0YWlubWVudHxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"));
         categoryRVAdapter.notifyDataSetChanged();
     }
 
-    private void getNews(String category){
-        loadingPB.setVisibility(View.VISIBLE);
+    private void getNews(String category) {
+//        loadingPB.setVisibility(View.VISIBLE);
+        animationView.setVisibility(View.VISIBLE);
         articlesArrayList.clear();
-        String categoryURL = "https://srd2705-news-api.herokuapp.com/update/"+category;
+        String categoryURL = "https://srd2705-news-api.herokuapp.com/update/" + category;
         String url = "https://srd2705-news-api.herokuapp.com/all_news";
         String BASE_URL = "https://srd2705-news-api.herokuapp.com/";
         Retrofit retrofit = new Retrofit.Builder().baseUrl(BASE_URL).addConverterFactory(GsonConverterFactory.create()).build();
         RetrofitAPI retrofitAPI = retrofit.create(RetrofitAPI.class);
         Call<NewsModal> call;
-        if(category.equals("all_news")){
+        if (category.equals("all_news")) {
             call = retrofitAPI.getAllNews(url);
-        }else {
+        } else {
             call = retrofitAPI.getNewsByCategory(categoryURL);
         }
 
@@ -77,10 +133,11 @@ public class MainActivity extends AppCompatActivity implements CategoryRVAdapter
             @Override
             public void onResponse(Call<NewsModal> call, Response<NewsModal> response) {
                 NewsModal newsModal = response.body();
-                loadingPB.setVisibility(View.GONE);
+//                loadingPB.setVisibility(View.GONE);
+                animationView.setVisibility(View.GONE);
                 ArrayList<Articles> articles = newsModal.getArticles();
-                for(int i=0; i<articles.size(); i++){
-                    articlesArrayList.add(new Articles(articles.get(i).getHead(),articles.get(i).getBody(),articles.get(i).getImg_link(),articles.get(i).getLink(),articles.get(i).getShort_body()));
+                for (int i = 0; i < articles.size(); i++) {
+                    articlesArrayList.add(new Articles(articles.get(i).getHead(), articles.get(i).getBody(), articles.get(i).getImg_link(), articles.get(i).getLink(), articles.get(i).getShort_body()));
 
                 }
                 newsRVAdapter.notifyDataSetChanged();
@@ -88,7 +145,7 @@ public class MainActivity extends AppCompatActivity implements CategoryRVAdapter
 
             @Override
             public void onFailure(Call<NewsModal> call, Throwable t) {
-                Toast.makeText(MainActivity.this, "Uh!!\nSomething went wrong\nWe are working on it", Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, "Uh!!\nIt's taking time\nWe are working on it", Toast.LENGTH_SHORT).show();
             }
         });
     }
@@ -99,3 +156,4 @@ public class MainActivity extends AppCompatActivity implements CategoryRVAdapter
         getNews(category);
     }
 }
+
