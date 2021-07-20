fae4eddf1781b3add5501661704d247d65b2ee94
srd2705 <souravdasrishi@gmail.com>
2021-07-20 17:22:43 +0530

    version 1.2.1 Build Splash screen Build SignIn/SignUp feature Build Logout feature Add loading animation Google login feature available Facebook and Phone login feature not available Firebase account: contactus@mininews@gmail.com

---
+++ app/src/main/java/com/example/newsapp/SplashActivity.java
@@ -0,0 +1,31 @@
+package com.example.newsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Thread td = new Thread(){
+            public void run(){
+                try {
+                    sleep(3000);
+                }catch (Exception ex){
+                    ex.printStackTrace();
+                }
+                finally {
+                    Intent intent = new Intent(SplashActivity.this,SignInActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        };
+        td.start();
+    }
+}
